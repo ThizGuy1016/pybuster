e904cbfde8194e8ebafce10db43eed40d824719e
ThizGuy1016 <vavraacademy@gmail.com>
2022-03-14 13:52:34 -0500

    readded error handling

--- pybuster.py
+++ pybuster.py
@@ -82,6 +82,6 @@ async def main() -> None:
 if __name__ == "__main__":
     try: asyncio.run(main())
     except KeyboardInterrupt: exit()
-    #except Exception as e:
-        #print(e)
-        #exit()
+    except Exception as e:
+        print(e)
+        exit()
